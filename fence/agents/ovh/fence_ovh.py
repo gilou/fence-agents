c28c3e283f3d794c5362b977ce4ff42b751f260f
Gilles Pietri <gpietri@dawan.fr>
2015-03-12 20:14:32 +0100

    Actually, this is probably not required, as we eventually log out

--- fence/agents/ovh/fence_ovh.py
+++ fence/agents/ovh/fence_ovh.py
@@ -42,7 +42,6 @@ def netboot_reboot(conn, options, mode):
 	conn.service.dedicatedHardRebootDo(options["session"],
 			options["--plug"], 'Fencing initiated by cluster', '', 'en')
 
-	conn.service.logout(options["session"])
 
 def reboot_time(conn, options):
 	result = conn.service.dedicatedHardRebootStatus(options["session"], options["--plug"])
